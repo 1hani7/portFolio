d41b6eff72174ce48b829b994268fbd7aca1b27d
1hani7 <1hani7@naver.com>
2023-10-27 09:36:20 +0900

    dsad

--- static/js/email.js
+++ static/js/email.js
@@ -1,23 +1,23 @@
-function sendMail(){
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("detail").value
-    };
+// function sendMail(){
+//     var params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         message: document.getElementById("detail").value
+//     };
     
-    const serviceID = 'service_e837yyo';
-    const templateID = 'template_hi7gl8k';
+//     const serviceID = 'service_e837yyo';
+//     const templateID = 'template_hi7gl8k';
     
-    emailjs.send(("service_e837yyo","template_hi7gl8k", params))
-    .then((res)=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("detail").value = "";
-        console.log(res);
-        alert("메일 전송이 완료되었습니다.")
-    })
-    .catch((err) => {
-        console.log(err);
-        alert("현재 기능 점검 중입니다.")
-    });
-}
+//     emailjs.send(("service_e837yyo","template_hi7gl8k", params))
+//     .then((res)=>{
+//         document.getElementById("name").value = "";
+//         document.getElementById("email").value = "";
+//         document.getElementById("detail").value = "";
+//         console.log(res);
+//         alert("메일 전송이 완료되었습니다.")
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//         alert("현재 기능 점검 중입니다.")
+//     });
+// }
